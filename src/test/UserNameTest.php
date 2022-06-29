28784019a7ccbf691a9943c79d123475d201e644
KatsuyaTanaka95 <gu0520kt@gmail.com>
2022-06-29 16:59:47 +0900

    テストコードとテストされるコードの追加

---
+++ src/test/UserNameTest.php
@@ -0,0 +1,28 @@
+<?php
+require_once __DIR__ . '/../vendor/autoload.php';
+
+use PHPUnit\Framework\TestCase;
+use App\Domain\ValueObject\UserName;
+
+final class UserNameTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function construct_(): void
+    {
+        $actual = new UserName('1234567890');
+
+        $this->assertSame('1234567890', $actual->value());
+    }
+
+    /**
+     * @test
+     */
+    public function construct_21文字以上ならエラー(): void
+    {
+        $this->expectException(InvariantException::class);
+
+        new UserName('123451234512345123451');
+    }
+}
