756519f2c88f830702efe84185532dbf3cbaae6e
KatsuyaTanaka95 <gu0520kt@gmail.com>
2022-07-01 13:39:23 +0900

    ディレクトリ構成の修正

--- src/test/Domain/ValueObject/UserNameTest.php
+++ src/test/Domain/ValueObject/UserNameTest.php
@@ -1,5 +1,5 @@
 <?php
-require_once __DIR__ . '/../vendor/autoload.php';
+require_once __DIR__ . '/../../../vendor/autoload.php';
 
 use PHPUnit\Framework\TestCase;
 use App\Domain\ValueObject\UserName;
